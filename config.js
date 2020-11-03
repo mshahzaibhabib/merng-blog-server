dcb7872d9dc5e5423805e6702e44baf1dbbd225b
mshahzaibhabib <mh.shahzaibhabib@gmail.com>
2020-11-03 13:36:58 +0500

    Removed config.js file

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://merng-social-user:Gi3giSV703qxq5D4@cluster0.cqewl.mongodb.net/merng?retryWrites=true&w=majority',
-    SECRET_KEY: 'some very secret key'
-}
